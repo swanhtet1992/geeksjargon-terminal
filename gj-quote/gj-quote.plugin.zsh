72bf90e4801a1f90138e3464f39171a757f80a06
SH <swanhtet1992@gmail.com>
2014-12-16 00:12:05 +0630

    Add author info and fix parsing issues

--- gj-quote/gj-quote.plugin.zsh
+++ gj-quote/gj-quote.plugin.zsh
@@ -6,10 +6,11 @@ AUTHOR_COLOR="\e[0;35m"
 
 function geeksjargon() {
     GJ=$(curl -s  --connect-timeout 2 "http://geeksjargon.com/quote/refresh")
-    QUOTE=$(echo $GJ | grep -m 1 "quote" | sed -e 's/.*\"quote\"//g' -e 's/<.*//g' -e 's/^[^a-zA-Z]*//' -e "s/&#039;/'/g" -e "s/&rsquo;/'/g")
+    QUOTE=$(echo $GJ | grep -m 1 "quote" | sed -e 's/.*\"quote\"//g' -e 's/<.*//g' -e 's/^[^a-zA-Z]*//' -e "s/&#039;/'/g" -e "s/&rsquo;/'/g" -e "s/&quot;/\`/g" -e "s/&hellip;/.../g")
     AUTHOR=$(echo $GJ | grep -m 1 "author_name" | sed -e 's/.*\"author_name\"//g' -e 's/<.*//g' -e 's/^[^a-zA-Z]*//')
+    INFO=$(echo $GJ | grep -m 1 "author_info" | sed -e 's/.*\"author_info\"//g' -e 's/<.*//g' -e 's/^[^a-zA-Z]*//' -e 's/}.*//' -e 's/&amp;/&/g') 
     if [ "$GJ" -a "$QUOTE" ]; then
-      echo "${QUOTE_COLOR}\"${QUOTE_COLOR}${QUOTE}\" - ${AUTHOR_COLOR}${AUTHOR}"
+	    echo "${QUOTE_COLOR}\"${QUOTE_COLOR}${QUOTE}\" - ${AUTHOR_COLOR}${AUTHOR} ( ${AUTHOR_COLOR}${INFO})"
     else
       geeksjargon
     fi
